2456669a085c8db523ff9820f2980be12eb5b78b
defrome <iskoblov555@gmail.com>
2025-12-18 13:40:18 +0300

    main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from contextlib import asynccontextmanager
+
+import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
@@ -27,3 +29,12 @@ app.add_middleware(
 app.include_router(master_router.router)
 app.include_router(application_router.router)
 app.include_router(services_router.router)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=True,
+        log_level="info"
+    )
