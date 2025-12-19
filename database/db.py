0f1937e20dec0eec581142901b8371199c94f9c3
defrome <iskoblov555@gmail.com>
2025-12-19 22:59:51 +0300

    optimization for docker

--- database/db.py
+++ database/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
+# Только такой путь для Docker!
 engine = create_engine("sqlite:////app/applications.db", connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autoflush=False, bind=engine)
